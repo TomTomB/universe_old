7fd4b5d0b2630b2803692489bf40baadba256ec9
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2021-01-24 15:06:26 +0100

    test: 💍 Fix not implemented error

---
+++ scripts/SetupJest.js
@@ -0,0 +1,3 @@
+// See: https://github.com/nickcolley/jest-axe/issues/147#issuecomment-758804533
+const { getComputedStyle } = window;
+window.getComputedStyle = (elt) => getComputedStyle(elt);
--- src/renderer/domain/UiKit/components/base/ScrollContainer/index.test.tsx
+++ src/renderer/domain/UiKit/components/base/ScrollContainer/index.test.tsx
@@ -3,13 +3,7 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
 import theme from '@styles/theme';
-import { configure } from '@testing-library/dom';
 import ScrollContainer from '.';
-
-// TODO(TRB): This should be inside a setup-tests.ts
-configure({
-  computedStyleSupportsPseudoElements: true,
-});
 
 describe('ScrollContainer', () => {
   it('should render', () => {
